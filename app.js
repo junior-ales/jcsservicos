af2dcf0c26a842f61438dac6bbcfcce80f49c330
Junior Ales <edilson.ales.jr@gmail.com>
2017-01-22 14:46:46 +0000

    adding different style to recommendation type when it is selected

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var PAGE = {
+    $recommendationTypes: document.querySelector('[data-recommendation-types]'),
+  };
+
+  var toggleRecommendationTypeClass = function() {
+    var selectedType = this.options[this.selectedIndex].value
+    this.className = selectedType === 'none' ? 'RecomendationType' : 'RecomendationType--active';
+  };
+
+  var init = function(page) {
+    page.$recommendationTypes.addEventListener('change', toggleRecommendationTypeClass);
+  };
+
+  init(PAGE);
+});
